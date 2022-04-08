e895ce559108383c5efd5faa6cbd9358bb92cff3
mhmoud <mhmoudsoliman41@outlook.com>
2022-04-08 21:07:30 +0200

    Location Commit

--- lib/home/homeScreen.dart
+++ lib/home/homeScreen.dart
@@ -8,7 +8,6 @@ import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
-import 'package:my_cab_driver/LocationService/LocationService.dart';
 import 'package:my_cab_driver/constance/constance.dart';
 import 'package:my_cab_driver/drawer/drawer.dart';
 import 'package:my_cab_driver/home/riderList.dart';
